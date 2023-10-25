100948cf60e12d110f5f7088290e2ba142dfb60c
Oladiya <baymyxy@gmail.com>
2023-10-25 16:03:49 +0300

    feat: add migrations

--- database/migrations/2023_10_25_115641_create_tasks_table.php
+++ database/migrations/2023_10_25_115641_create_tasks_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
             $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->longText('description');
+            $table->timestamp('start')->nullable();
+            $table->timestamp('end');
+            $table->unsignedInteger('priority');
+            $table->string('status');
+            $table->unsignedInteger('category_id');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('tasks');
     }
 };
