0721e1289e59f924ed0147c1477c496630ec8e77
Oladiya <baymyxy@gmail.com>
2023-10-25 18:51:17 +0300

    feat: add authentication
    add authentication and bootstrap

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('surname')->nullable();
+            $table->string('middle_name')->nullable();
             $table->string('login')->unique();
             $table->string('email')->unique();
             $table->boolean('is_admin')->nullable();
